15ccd27162c5a0b8d4f0776c1ae885e93650084d
Benjamin Watson <BWatson@nfu.org.uk>
2019-01-29 15:15:44 +0000

    Override GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,5 +8,43 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count<5)
+            {
+                throw new System.InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            int sRank = Students.Count + 1;
+
+            foreach (Student s in Students)
+            {
+
+                if (averageGrade > s.AverageGrade) { sRank -= 1; }
+
+            }
+
+            float fRank = sRank / Students.Count;
+
+            if (fRank <= 0.2)
+            {
+                return 'A';
+            } else if(fRank <= 0.4)
+            {
+                return 'B';
+            } else if(fRank <= 0.6)
+            {
+                return 'C';
+            } else if(fRank <= 0.8)
+            {
+                return 'D';
+            } else
+            {
+                return 'F';
+            }
+
+        }
+
     }
 }
